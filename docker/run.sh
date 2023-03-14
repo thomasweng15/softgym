144119caa94a06b84b9acda83fde3d54d63c2c6c
Thomas Weng <thomas.weng11@gmail.com>
2023-03-14 17:40:07 -0400

    build 3d cloth env

---
+++ docker/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+docker run \
+    -v $SCRIPT_DIR/..:/workspace/softgym \
+    -v $HOME/miniconda3:$HOME/miniconda3 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --gpus all \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    -it xingyu/softgym:latest bash
