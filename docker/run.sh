fdb007d53746db5ea1ef33a66e4129103424ccda
Thomas Weng <thomas.weng11@gmail.com>
2023-03-27 18:32:45 -0400

    Upgrade to python 3.7

--- docker/run.sh
+++ docker/run.sh
@@ -7,6 +7,7 @@ docker run \
     -v $HOME/miniconda3:$HOME/miniconda3 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     --gpus all \
+    -e PATH=$HOME/miniconda3/bin:$PATH \
     -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -it xingyu/softgym:latest bash
