1d6fa307da73a4d019e423a6a2f8c7dc336f9636
Thomas Weng <thomas.weng11@gmail.com>
2023-11-09 23:25:28 -0500

    update conda env name

--- prepare_1.0.sh
+++ prepare_1.0.sh
@@ -1,4 +1,4 @@
-. activate softgym3d_py37
+. activate hacman
 export PYFLEXROOT=${PWD}/PyFlex
 export PYTHONPATH=${PYFLEXROOT}/bindings/build:$PYTHONPATH
 export LD_LIBRARY_PATH=${PYFLEXROOT}/external/SDL2-2.0.4/lib/x64:$LD_LIBRARY_PATH
